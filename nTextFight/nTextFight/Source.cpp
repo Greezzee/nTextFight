b875687e586a91e40a37322767669d8141a5c90d
Евгений Зорин <zorin.eugene@gmail.com>
2019-03-18 20:37:55 +0600

    add player files and miss animation

--- nTextFight/nTextFight/Source.cpp
+++ nTextFight/nTextFight/Source.cpp
@@ -4,12 +4,13 @@
 #include <iterator>
 #include <string>
 #include <cmath>
+#include <fstream>
 #include <SFML/Graphics.hpp>
 #define uint unsigned int
 using namespace std;
 
 int msStepTime = 500000;
-
+const float PI = 3.1415926f;
 
 struct attackPackage {
 	int DMG;
@@ -19,12 +20,22 @@ struct effect {
 	int ID, duration = 0;
 	int level;
 };
-struct skills {
-	int ID, 
+struct skill {
+	int ID;
 };
 struct stats
 {
 	int strenght, agility, accuracy, intelligence;
+	int baseHP, baseDMG;
+	int realHP, realDMG;
+	int currentHP;
+};
+struct playerInfo {
+	string name, spriteName;
+	stats playerStats;
+	sf::Vector2i startPos;
+	vector<skill> skills;
+	int id;
 };
 
 float sigmoid(float x) {
@@ -33,74 +44,92 @@ float sigmoid(float x) {
 
 class player {
 public:
-	player(string name, string team, string spriteName, sf::Vector2i startPos, int id) {
+	player(playerInfo input) {
 
-		myID = id;
+		myID = input.id;
 
-		myTexture.loadFromFile("./Sprites/Characters/" + spriteName + ".png");
+		myTexture.loadFromFile("./Sprites/Characters/" + input.spriteName + ".png");
 		punchTexture.loadFromFile("./Sprites/Effects/std_punch.png");
 		mySprite.setTexture(myTexture);
 		punchSprite.setTexture(punchTexture);
 
-		baseHP = 100;
-		baseDMG = 10;
-		realHP = baseHP;
-		currentHP = realHP;
-		realDMG = baseDMG;
-		myStats.accuracy = 100;
-		myStats.agility = 0;
-		myStats.intelligence = 50;
-		myStats.strenght = 50;
+		myStats = input.playerStats;
+		myStats.realHP = myStats.baseHP;
+		myStats.currentHP = myStats.realHP;
+		myStats.realDMG = myStats.baseDMG;
 
-		myTeam = team;
-		myName = name;
-		position = startPos;
+		myName = input.name;
+		position = input.startPos;
 
 		effects.resize(0);
 
 		isAlive = true;
 		isActionDone = false;
-
-		if (myTeam == "red") spritePos = sf::Vector2i(256 - 64 * position.x, 16 + 128 * position.y);
-		if (myTeam == "blue") spritePos = sf::Vector2i(256 + 64 * position.x, 16 + 128 * position.y);
 	}
 
 	void action(vector<player*> enemy, sf::RenderWindow *window, sf::Time thisStep, bool isNewStep) {
 		
-		if (isNewStep) target = chooseTarget(enemy);
+		if (isNewStep) {
+			target = chooseTarget(enemy);
+			myAttack.DMG = myStats.realDMG;
+			myAttack.successChange = myStats.accuracy;
+			isAttackSuccess = !target->testForMiss(myAttack.successChange);
+		}
 
-		punchSprite.setTextureRect(sf::IntRect(64 * (int)(thisStep.asMicroseconds() * 4 / msStepTime), 0, 64, 64));
-		punchSprite.setPosition(target->getPosition().x - 16, target->getPosition().y);
-		window->draw(punchSprite);
+		if (isAttackSuccess) {
+			punchSprite.setTextureRect(sf::IntRect(64 * (int)(thisStep.asMicroseconds() * 4 / msStepTime), 0, 64, 64));
+			punchSprite.setPosition(target->getPosition().x - 16, target->getPosition().y);
+			window->draw(punchSprite);
+		}
+		else {
+			sf::Font font;
+			font.loadFromFile("font.ttf");
+			sf::Text evadeText("Evade!", font);
+			int alpha = 255;
+			int test = thisStep.asMicroseconds();
+			if ((float)(msStepTime - test) / (float)msStepTime <= 0.25)
+				alpha = (int)(((float)(msStepTime - test) / (float)msStepTime * 4) * 255.f);
+			if ((float)(msStepTime - test) / (float)msStepTime >= 0.75)
+				alpha = (int)(((1.f - (float)abs(test - msStepTime) / (float)msStepTime) * 4) * 255.f);
+			evadeText.setFillColor(sf::Color(255, 255, 255, alpha));
+			evadeText.setOutlineColor(sf::Color(255, 255, 255, alpha));
+			evadeText.setPosition(target->getPosition().x - 36, target->getPosition().y + 24 - 16 * sin((float)thisStep.asMicroseconds() / (float)msStepTime * PI));
+			window->draw(evadeText);
+		}
+
 
 		sf::RectangleShape aura(sf::Vector2f(36, 68));
 		aura.setFillColor(sf::Color(0, 0, 0, 0));
 		aura.setOutlineColor(sf::Color(255, 255, 255, 255));
 		aura.setOutlineThickness(-2);
-		aura.setPosition(spritePos.x - 2, spritePos.y - 2);
+		aura.setPosition((float)spritePos.x - 2.f, (float)spritePos.y - 2.f);
 		window->draw(aura);
 
 
 
-		if (!isActionDone && thisStep.asMicroseconds() > msStepTime / 2) {
-			attackPackage myAttack;
-			myAttack.DMG = realDMG;
-			myAttack.successChange = myStats.accuracy;
-			cout << myName + "(" + to_string(currentHP) + ")" << " attack ";
+		if (!isActionDone && thisStep.asMicroseconds() > msStepTime / 2 && isAttackSuccess) {
+			cout << myName + "(" + to_string(myStats.currentHP) + ")" << " attack ";
 			target->takeAttack(myAttack);
+			isActionDone = true;
+		}
+		else if (!isActionDone && thisStep.asMicroseconds() > msStepTime / 2) {
+			cout << myName + "(" + to_string(myStats.currentHP) + ")" << " attack ";
+			cout << "but " << target->getName() << " evade" << endl;
 			isActionDone = true;
 		}
 	}
 
 	void update(sf::RenderWindow *window) {
 
-		mySprite.setPosition(spritePos.x, spritePos.y);
+		if (myTeam == "red") spritePos = sf::Vector2i(280 - 80 * (position.x + 1), 16 + 128 * position.y);
+		if (myTeam == "blue") spritePos = sf::Vector2i(280 + 80 * (position.x + 1), 16 + 128 * position.y);
+		mySprite.setPosition((float)spritePos.x, (float)spritePos.y);
 		window->draw(mySprite);
 
 		sf::RectangleShape healthBar(sf::Vector2f(64, 10));
-		sf::RectangleShape activeHeathBar(sf::Vector2f((int)(64 * ((float)currentHP / (float)realHP)), 10));
-		healthBar.setPosition(spritePos.x - 16, spritePos.y + 74);
-		activeHeathBar.setPosition(spritePos.x - 16, spritePos.y + 74);
+		sf::RectangleShape activeHeathBar(sf::Vector2f((float)((int)(64 * ((float)myStats.currentHP / (float)myStats.realHP))), 10.f));
+		healthBar.setPosition((float)spritePos.x - 16.f, (float)spritePos.y + 74.f);
+		activeHeathBar.setPosition((float)spritePos.x - 16.f, (float)spritePos.y + 74.f);
 		activeHeathBar.setFillColor(sf::Color::Red);
 		window->draw(healthBar);
 		window->draw(activeHeathBar);
@@ -111,11 +140,9 @@ public:
 	}
 
 	void takeAttack(attackPackage attack) {
-		if (!testForMiss(attack.successChange)) {
-			cout << "and " << myName + "(" + to_string(currentHP) + "->" + to_string(currentHP - attack.DMG) + ")" << " take " << attack.DMG << " damage" << endl;
-			currentHP -= attack.DMG;
-			if (currentHP <= 0) isAlive = false;
-		}
+		cout << "and " << myName + "(" + to_string(myStats.currentHP) + "->" + to_string(myStats.currentHP - attack.DMG) + ")" << " take " << attack.DMG << " damage" << endl;
+		myStats.currentHP -= attack.DMG;
+		if (myStats.currentHP <= 0) isAlive = false;
 	}
 	void activateEffects() {
 		for (auto i = effects.begin(); i != effects.end(); i++) {
@@ -126,8 +153,8 @@ public:
 				{
 				case 1:
 					if (i->duration == 0) i->duration = 3;
-					currentHP += 5;
-					if (currentHP > realHP) currentHP = realHP;
+					myStats.currentHP += 5;
+					if (myStats.currentHP > myStats.realHP) myStats.currentHP = myStats.realHP;
 					cout << myName + " regenerate 5 HP" << endl;
 					break;
 				}
@@ -141,23 +168,36 @@ public:
 		effects = newEffects;
 	}
 
+	void setTeam(string team) {
+		myTeam = team;
+	}
 	bool getAlive() {
 		return isAlive;
 	}
 	sf::Vector2f getPosition() {
 		return mySprite.getPosition();
 	}
-
+	stats getStats() {
+		return myStats;
+	}
+	string getName() {
+		return myName;
+	}
+	bool testForMiss(int successChange) {
+		float realChange = (float)(myStats.agility - successChange) / 25.f;
+		realChange = sigmoid(realChange) * 100;
+		if (rand() % 100 + 1 < realChange) return true;
+		else return false;	
+	}
 private:
 	int myID;
-	int baseHP, baseDMG;
-	int realHP, realDMG;
-	int currentHP;
-	bool isAlive, isActionDone;
+	bool isAlive, isActionDone, isAttackSuccess;
 	stats myStats;
+	attackPackage myAttack;
 	string myTeam;
 	string myName;
 	list<effect> effects;
+	vector<skill> mySkils;
 	sf::Vector2i position;
 	sf::Texture myTexture, punchTexture;
 	sf::Sprite mySprite, punchSprite;
@@ -172,17 +212,6 @@ private:
 		}
 		return enemy[0];
 	}
-	bool testForMiss(int successChange) {
-		float realChange = (float)(myStats.agility - successChange) / 50.f;
-		realChange = sigmoid(realChange) * 100;
-		if (rand() % 100 + 1 < realChange) {
-			cout << "but " << myName << " evade" << endl;
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
 };
 
 int testForAlive(vector<player*> redTeam, vector<player*> blueTeam) {
@@ -194,32 +223,45 @@ int testForAlive(vector<player*> redTeam, vector<player*> blueTeam) {
 	if (!isAnyBlueAlive && !isAnyBlueAlive) return -1;
 	return 0;
 }
+playerInfo readPlayerFile(ifstream *file) {
+	playerInfo out;
+	*file >> out.spriteName;
+	*file >> out.playerStats.baseHP >> out.playerStats.baseDMG;
+	*file >> out.playerStats.strenght >> out.playerStats.agility >> out.playerStats.intelligence >> out.playerStats.accuracy;
+	return out;
+}
+
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(512, 512), "nText Fight 0.0.1");
-	vector<player*> redTeam(2);
-	vector<player*> blueTeam(2);
-	for (uint i = 0; i < redTeam.size(); i++) {
-		if (i == 0) {
-			player *a = new player("lupa" + to_string(i), "red", "warrior", sf::Vector2i(1, 1 + i), i);
-			redTeam[i] = a;
+	vector<player*> redTeam(0);
+	vector<player*> blueTeam(0);
+
+	ifstream battleFile;
+	battleFile.open("data/battle");
+	string input = "";
+	for (int i = 0; i < 4; i++) for (int j = 0; j < 6; j++) {
+		battleFile >> input;
+		if (input == "-") continue;
+
+		ifstream playerFile;
+		playerFile.open("data/Players/" + input + ".txt");
+		playerInfo info = readPlayerFile(&playerFile);
+		info.name = input;
+		info.id = i * 6 + j;
+		if (j < 3) {
+			info.startPos = sf::Vector2i(2 - j, i);
+			player *a = new player(info);
+			a->setTeam("red");
+			redTeam.push_back(a);
 		}
 		else {
-			player *a = new player("lupa" + to_string(i), "red", "archer", sf::Vector2i(1, 1 + i), i);
-			redTeam[i] = a;
+			info.startPos = sf::Vector2i(j - 3, i);
+			player *a = new player(info);
+			a->setTeam("blue");
+			blueTeam.push_back(a);
 		}
 	}
 
-	for (uint i = 0; i < blueTeam.size(); i++) {
-		if (i == 1) {
-			player *a = new player("pupa" + to_string(i), "blue", "warrior", sf::Vector2i(1, 1 + i), i);
-			blueTeam[i] = a;
-		}
-		else {
-			player *a = new player("pupa" + to_string(i), "blue", "mage", sf::Vector2i(1, 1 + i), i);
-			blueTeam[i] = a;
-		}
-	}
 
 	sf::Clock clock;
 	sf::Time timer, stepTimer;
@@ -227,6 +269,9 @@ int main() {
 	int activePlayerID = 0;
 	string activeTeam = "red";
 	bool isNewStep = true;
+
+
+	sf::RenderWindow window(sf::VideoMode(560, 512), "nText Fight 0.0.1");
 	while (window.isOpen()) {
 		timer += clock.getElapsedTime();
 		stepTimer += timer;
